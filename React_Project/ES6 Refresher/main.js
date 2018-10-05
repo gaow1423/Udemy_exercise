831dcc7bdc207ccae186baa33a4e054b7dd0576e
Wei Gao <gaow1423@gmail.com>
2018-10-04 22:59:37 -0500

    d
    d

---
+++ React_Project/ES6 Refresher/main.js
@@ -0,0 +1,21 @@
+//CONST & LET
+
+let name = 'John';
+name = 'Jack';
+
+//const object or const array can be manipulated
+const person = {
+  name: 'John',
+  age: 33
+};
+
+person.name = 'Jack';
+const nums = [1, 2, 3, 4];
+nums.push(5);
+
+//ARROW FUNCTIONS
+
+//FOREACH
+//MAP
+//FILTER
+//SPREAD
