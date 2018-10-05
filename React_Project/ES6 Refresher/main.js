61d9b1ddda3068ea13a5a35d8b966efc80c74ede
Wei Gao <gaow1423@gmail.com>
2018-10-05 12:35:47 -0500

    d
    d

--- React_Project/ES6 Refresher/main.js
+++ React_Project/ES6 Refresher/main.js
@@ -54,44 +54,44 @@
 // consule.log(arr3);
 // console.log(Person2);
 
-//Destructuring
-const profile = {
-  name: 'John Doe',
-  address: {
-    street: '40 Main st',
-    city: 'Boston'
-  },
-  hobbies: ['movies', 'music']
-};
-const { name, address, hobbies } = profile; //pull name, address, and hobbies out.
+// //Destructuring
+// const profile = {
+//   name: 'John Doe',
+//   address: {
+//     street: '40 Main st',
+//     city: 'Boston'
+//   },
+//   hobbies: ['movies', 'music']
+// };
+// const { name, address, hobbies } = profile; //pull name, address, and hobbies out.
 
-console.log(name, address.street, hobbies[0]);
+// console.log(name, address.street, hobbies[0]);
 
-//ClASSES
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  greet() {
-    return `Hello. my name is ${this.name} and I am ${this.age}`;
-  }
-}
-const person1 = new Person('John', 33);
-const person2 = new Person('Sara', 28);
+// //ClASSES
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   greet() {
+//     return `Hello. my name is ${this.name} and I am ${this.age}`;
+//   }
+// }
+// const person1 = new Person('John', 33);
+// const person2 = new Person('Sara', 28);
 
-console.log(person2.greet());
+// console.log(person2.greet());
 
-//SUBCLASS
-class Customer extends Person {
-  constructor(name, age, balance) {
-    super(name, age);
-    this.balance = balance;
-  }
-  info() {
-    return `${this.name} owes ${this.balance}`;
-  }
-}
+// //SUBCLASS
+// class Customer extends Person {
+//   constructor(name, age, balance) {
+//     super(name, age);
+//     this.balance = balance;
+//   }
+//   info() {
+//     return `${this.name} owes ${this.balance}`;
+//   }
+// }
 
-const customer1 = new Customer('Kevin', 32, 300);
-console.log(customer1.info());
+// const customer1 = new Customer('Kevin', 32, 300);
+// console.log(customer1.info());
