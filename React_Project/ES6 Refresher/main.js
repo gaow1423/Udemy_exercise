bac2b6503181d7dfb51338a93fe00548546fb9f6
Wei Gao <gaow1423@gmail.com>
2018-10-06 10:57:28 -0500

    d
    d

--- React_Project/ES6 Refresher/main.js
+++ React_Project/ES6 Refresher/main.js
@@ -67,36 +67,36 @@
 
 // console.log(name, address.street, hobbies[0]);
 
-// //ClASSES
-// class Person {
-//   constructor(name, age) {
-//     this.name = name;
-//     this.age = age;
-//   }
-//   greet() {
-//     return `Hello. my name is ${this.name} and I am ${this.age}`;
-//   }
-// }
+//ClASSES
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greet() {
+    return `Hello. my name is ${this.name} and I am ${this.age}`;
+  }
+}
 // const person1 = new Person('John', 33);
 // const person2 = new Person('Sara', 28);
 
 // console.log(person2.greet());
 
-// //SUBCLASS
-// class Customer extends Person {
-//   constructor(name, age, balance) {
-//     super(name, age);
-//     this.balance = balance;
-//   }
-//   info() {
-//     return `${this.name} owes ${this.balance}`;
-//   }
-// }
+//SUBCLASS
+class Customer extends Person {
+  constructor(name, age, balance) {
+    super(name, age);
+    this.balance = balance;
+  }
+  info() {
+    return `${this.name} owes ${this.balance}`;
+  }
+}
 
-// const customer1 = new Customer('Kevin', 32, 300);
-// console.log(customer1.info());
+const customer1 = new Customer('Kevin', 32, 300);
+console.log(customer1.info());
 
-//MODULES
+// //MODULES
 
 // //file 1 (file1.js)
 // export const name = 'Jeff';
